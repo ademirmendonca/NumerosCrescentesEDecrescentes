79854c230e3ffe9492a2be95e204bd1261e5bc13
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-05-19 17:31:26 -0300

    Add project files.

---
+++ NumerosCrescentesEDecrescentes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NumerosCrescentesEDecrecentesURI1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            //Recorta a string com os numeros inseridos
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            //Verifica a condicao e imprime o resultado da ordem
+            //se os valores de x e y forem iguais, encerra a aplicacao
+            while (x != y)
+            {
+
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+
+        }
+    }
+}
